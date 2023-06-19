9bcde8b2d150d16b2d4292004a750e96e4087e3e
Mufeed Oomatia <MOomatia@quantium.com>
2023-06-19 12:49:12 +1000

    Made a calendar and initial task

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import WeekView from "./calendar";
+import moment from 'moment';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Morning Run",
+      description: "5k run around the park",
+      startTime: moment().hour(10).minute(0),
+      endTime: moment().hour(8).minute(30),
+      day: moment().dayOfYear(),
+    },
+    // ... more tasks
+  ]);
+
   return (
-    <WeekView/>
+    <WeekView tasks={tasks}/>
   );
 }
 
--- frontend/client/src/calendar.js
+++ frontend/client/src/calendar.js
@@ -2,15 +2,18 @@ import React, { useEffect, useRef, useState } from 'react';
 import moment from 'moment';
 import { Button, Typography, Box, ButtonGroup, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'; 
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import CalendarTasks from './calendarTasks'
 
+const lightGrey = '#EEEEEE'
+const slotHeight = 100
 const useStyles = makeStyles((theme) => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    maxWidth: 800,
+    maxWidth: 1500,
     margin: '0 auto',
     marginTop: theme.spacing(2),
   },
@@ -18,33 +21,35 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
-    backgroundColor: theme.palette.grey[200],
     textAlign: 'center',
     fontWeight: 'bold',
-    borderBottom: `1px solid ${theme.palette.divider}`,
+    borderBottom: `2px solid ${lightGrey}`,
     padding: theme.spacing(1),
+  },
+  greyOutline: {
+    outline: `2px solid ${lightGrey}`
   },
   header: {
     display: 'flex',
-    backgroundColor: theme.palette.grey[200],
     textAlign: 'center',
     fontWeight: 'bold',
-    borderBottom: `1px solid ${theme.palette.divider}`,
-    padding: theme.spacing(1),
   },
   dayHeader: {
     flex: 1,
     width: 50,
     padding: theme.spacing(1),
+    borderBottom: `2px solid ${lightGrey};`,
   },
   timeHeader: {
-    borderRight: `1px solid ${theme.palette.divider}`,
+    borderRight: `2px solid ${lightGrey};`,
     padding: theme.spacing(1),
+    minWidth: 45,
+    borderBottom: `2px solid ${lightGrey};`,
   },
   timeColumn: {
     display: 'flex',
     flexDirection: 'column',
-    width: 50,
+    minWidth: 50
   },
   dayColumn: {
     flex: 1,
@@ -53,9 +58,19 @@ const useStyles = makeStyles((theme) => ({
   },
   timeSlot: {
     flex: 1,
-    minHeight: 50,
-    borderTop: `1px solid ${theme.palette.divider}`,
-    borderLeft: `1px solid ${theme.palette.divider}`,
+    minHeight: 120,
+    borderTop: `2px solid ${lightGrey}`,
+    borderRight: `2px solid ${lightGrey}`, 
+    padding: theme.spacing(1),
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  edgeTimeSlot: {
+    flex: 1,
+    minHeight: 120,
+    borderTop: `2px solid ${lightGrey}`,
     padding: theme.spacing(1),
     position: 'relative',
     display: 'flex',
@@ -68,11 +83,40 @@ const useStyles = makeStyles((theme) => ({
     right: 0,
     borderTop: `2px solid ${theme.palette.error.main}`,
   },
+  dottedredLine: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    borderTop: `2px dotted ${theme.palette.error.main}`,
+  },
+  beforedottedredLine: {
+    '&::before': {
+        content: '""',
+        position: 'absolute',
+        top: '-5px',
+        left: 0,
+        width: '10px',
+        height: '10px',
+        borderRadius: '50%',
+        backgroundColor: theme.palette.error.main,
+      },
+  },
+  afterdottedredLine: {
+    '&::after': {
+        content: '""',
+        position: 'absolute',
+        bottom: '-5px',
+        right: 0,
+        width: '10px',
+        height: '10px',
+        borderRadius: '50%',
+        backgroundColor: theme.palette.error.main,
+      },
+  },
   weekSelector: {
     display: 'flex',
     justifyContent: 'center',
     padding: theme.spacing(2),
-    backgroundColor: theme.palette.grey[200],
     alignItems: 'center',
     '& > *': {
       margin: theme.spacing(0, 1),
@@ -80,19 +124,49 @@ const useStyles = makeStyles((theme) => ({
   },
   calendarContainer: {
     display: 'flex',
-    height: 600,
+    height: 900,
     overflowY: 'scroll',
+  },
+  taskBox: {
+    position: 'absolute',
+    width: '87%',
+    marginTop: '3%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.spacing(1),
+    borderRadius: '10px 10px 10px 10px'
   },
 }));
 
-function Calendar() {
+function Calendar({tasks}) {
+  console.log(tasks)
   const classes = useStyles();
   const [now, setNow] = useState(moment());
   const [startOfWeek, setStartOfWeek] = useState(moment().startOf('week'));
   const currentTimeSlotRef = useRef(null);
-  const days = Array.from({ length: 7 }, (_, i) => startOfWeek.clone().add(i, 'day'));
+  const [days, setDays] = useState([]) ;
+  
+  useEffect(() => {
+    const sevenDayList = Array.from({ length: 7 }, (_, i) => startOfWeek.clone().add(i, 'day'));
+    setDays(sevenDayList)
+  }, [startOfWeek])
 
   const minutePercent = (now.minutes() / 60) * 100;
+  const [selected, setSelected] = React.useState('Week');
+
+  const buttons = ['Day', 'Week', 'Month'];
+
+  const changeFormat = (button) => {
+    setSelected(button)
+    if(button==='Day'){
+      setDays([now])
+    }else if(button==='Week'){
+
+      const sevenDayList = Array.from({ length: 7 }, (_, i) => startOfWeek.clone().add(i, 'day'));
+      setDays(sevenDayList)
+    }
+  }
 
   const goNextWeek = () => {
     setStartOfWeek(startOfWeek.clone().add(1, 'week'));
@@ -113,73 +187,106 @@ function Calendar() {
     if (currentTimeSlotRef.current && now.isSame(startOfWeek, 'week')) {
       currentTimeSlotRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-  }, [now, startOfWeek]);
+  });
 
+  
   return (
-    <Box className={classes.container}>
-    <Box className={classes.top}>
-        <Box className={classes.weekSelector}>
-            <IconButton color="primary" onClick={goPrevWeek}>
-            <ArrowBackIosIcon />
-            </IconButton>
-            <Typography variant="h6">
-            {`${startOfWeek.format('MMMM D')} - ${startOfWeek.clone().add(6, 'days').format('MMMM D')}`}
+        <Box className={classes.container}>
+        <Box className={classes.top}>
+            <Box className={classes.weekSelector}>
+                <IconButton color="primary" onClick={goPrevWeek} className={classes.greyOutline}>
+                <NavigateBeforeIcon/>
+                </IconButton>
+                <Typography variant="h6">
+                {`${startOfWeek.format('MMMM D')} - ${startOfWeek.clone().add(6, 'days').format('MMMM D')}`}
+                </Typography>
+                <IconButton color="primary" onClick={goNextWeek} className={classes.greyOutline}>
+                <NavigateNextIcon/>
+                </IconButton>
+            </Box>
+
+            <ButtonGroup color="secondary" aria-label="text button group">
+                {buttons.map((button, index) => (
+                    <Button style={{padding: '5px 15px', margin: '1px'}}
+                    variant={selected === button ? 'contained' : 'text'}
+                    color={selected === button ? 'primary' : 'default'}
+                    onClick={() => changeFormat(button)}
+                    className={classes.greyOutline}
+                    key={index}
+                    >
+                    {button}
+                    </Button>
+                ))}
+              </ButtonGroup>
+        </Box>
+        <Box className={classes.header}>
+            <Typography className={classes.timeHeader}>
+                <AccessTimeIcon/>
             </Typography>
-            <IconButton color="primary" onClick={goNextWeek}>
-            <ArrowForwardIosIcon/>
-            </IconButton>
+            {days.map((date) => (
+            <Typography className={classes.dayHeader} key={date.format()}>
+                {date.format('ddd D')}
+            </Typography>
+            ))}
         </Box>
 
-        <ButtonGroup variant="outlined" aria-label="outlined button group">
-            <Button>Day</Button>
-            <Button>Week</Button>
-            <Button>Month</Button>
-        </ButtonGroup>
-    </Box>
+        <Box className={classes.calendarContainer}>
+            <Box className={classes.timeColumn}>
+            {Array.from({ length: 24 }, (_, i) => (
+                <Typography className={classes.timeSlot} key={i}>
+                {`${moment(i, 'H').format('h a')}`}
+                </Typography>
+            ))}
+            </Box>
 
-
-      <Box className={classes.header}>
-        <Typography className={classes.timeHeader}>
-            <AccessTimeIcon/>
-        </Typography>
-        {days.map((date) => (
-          <Typography className={classes.dayHeader} key={date.format()}>
-            {date.format('ddd D')}
-          </Typography>
-        ))}
-      </Box>
-
-      <Box className={classes.calendarContainer}>
-        <Box className={classes.timeColumn}>
-          {Array.from({ length: 24 }, (_, i) => (
-            <Typography className={classes.timeSlot} key={i}>
-              {`${i}:00`}
-            </Typography>
-          ))}
-        </Box>
-
-        {days.map((date) => (
-          <Box className={classes.dayColumn} key={date.format()}>
-            {Array.from({ length: 24 }, (_, j) => (
-              <Box
-                className={classes.timeSlot}
-                key={j}
-                ref={now.isSame(date, 'day') && now.hour() === j ? currentTimeSlotRef : null}
-              >
-                {now.isSame(date, 'day') && now.hour() === j && (
+            {days.map((date) => (
+            <Box className={classes.dayColumn} key={date.format()}>
+                {
+                Array.from({ length: 24 }, (_, j) => (
                   <Box
-                    className={classes.redLine}
-                    style={{
-                      top: `${minutePercent}%`,
-                    }}
-                  />
-                )}
+                      className={date.day() === days.length - 1? classes.edgeTimeSlot : classes.timeSlot }
+                      key={j}
+                      ref={now.isSame(date, 'day') && now.hour() === j ? currentTimeSlotRef : null}
+                  >
+                    
+                      {now.isSame(date, 'day') && now.hour() === j && (
+                      <Box
+                          className={classes.redLine}
+                          style={{
+                          top: `${minutePercent}%`,
+                          
+                          }}
+                      />
+                      )}
+
+                      {now.isSame(date, 'day') && now.hour() === j && (
+                        <Box
+                          className={classes.taskBox}
+                          style={{
+                            height: `150px`,
+                            top: `30%`,
+                            backgroundColor: '#00FF000F',
+                            border: '2px solid #00FF00',
+                            }}
+                      >run</Box>
+                      )}
+
+                      {now.hour() === j && (
+                      <Box
+                          className={classes.dottedredLine}
+                          style={{
+                          top: `${minutePercent}%`,
+                          }}
+                      />
+                      )}
+                      
+                  </Box>
+                  ))}
               </Box>
             ))}
-          </Box>
-        ))}
-      </Box>
-    </Box>
+            
+        </Box>
+        </Box>
   );
 }
 
